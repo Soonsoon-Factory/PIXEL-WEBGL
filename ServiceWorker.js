84c95db45595ee22046f5a8df2b24b98d3c0ae2c
SpumNet <astrovus@soonsoons.com>
2024-10-25 10:40:48 +0900

    ds

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
 
-const CACHE_VERSION = '0.4_2024.10.25.10.32';
+const CACHE_VERSION = '0.4_2024.10.25.10.40';
 const CACHE_NAME = `game-cache-${CACHE_VERSION}`;
 
 const CACHE_TARGETS = [
@@ -31,21 +31,21 @@ self.addEventListener('install', event => {
         })
     );
 });
-
 self.addEventListener('fetch', event => {
     event.respondWith(
         fetch(event.request)
             .then(response => {
-                // framework.js는 항상 네트워크에서 가져오기
                 if (event.request.url.includes('.framework.js')) {
                     return response;
                 }
-                
+
                 const clonedResponse = response.clone();
                 caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, clonedResponse);
+                    if (/^https?:$/.test(new URL(event.request.url).protocol)) {
+                        cache.put(event.request, clonedResponse);
+                    }
                 });
-                
+
                 return response;
             })
             .catch(() => {
